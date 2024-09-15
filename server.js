8c1f5f536b048923744a62931c3777e8dc4b82ae
TejamReddyK <118739275+TejamReddyK@users.noreply.github.com>
2024-09-15 22:53:03 +0530

    Loading data from JSON file

--- server.js
+++ server.js
@@ -6,23 +6,6 @@ const budget = require('./budgetData.json')
 
 app.use('/', express.static('public'));
 
-// const budget={
-//  myBudget: [
-//     {
-//         title: 'Eat out',
-//         budget: 30
-//     },
-//     {
-//         title: 'Rent',
-//         budget: 350
-//     },
-//     {
-//         title: 'Groceries',
-//         budget: 90
-//     },
-// ]
-// };
-
 app.get('/hello', (req,res) =>{
     res.send('Hello World!');
 });
