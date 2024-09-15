5d6237f6b676c8b6df7a5bf22fd93154689d5662
TejamReddyK <118739275+TejamReddyK@users.noreply.github.com>
2024-09-15 22:51:48 +0530

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -2,24 +2,26 @@ const express= require('express');
 const app=express();
 const port=3000;
 
+const budget = require('./budgetData.json')
+
 app.use('/', express.static('public'));
 
-const budget={
- myBudget: [
-    {
-        title: 'Eat out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-]
-};
+// const budget={
+//  myBudget: [
+//     {
+//         title: 'Eat out',
+//         budget: 30
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 350
+//     },
+//     {
+//         title: 'Groceries',
+//         budget: 90
+//     },
+// ]
+// };
 
 app.get('/hello', (req,res) =>{
     res.send('Hello World!');
