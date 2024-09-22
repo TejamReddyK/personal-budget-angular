4937e5a45f4c3e2010688e18f4d5e8492d698ebb
TejamReddyK <118739275+TejamReddyK@users.noreply.github.com>
2024-09-22 18:32:16 -0400

    Update homepage.component.ts

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,50 +1,49 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Chart, ChartItem } from 'chart.js/auto';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
 
-  public dataSource:any =  {
+  public dataSource: any = {
     datasets: [
-        {
-            data: [],
-            backgroundColor: ['#ffcd56', '#ff6384', '#36a2eb', '#fd6b19', '#ababab', '#a03291', '#2910de'],
-        }
+      {
+        data: [],
+        backgroundColor: ['#ffcd56', '#ff6384', '#36a2eb', '#fd6b19', '#ababab', '#a03291', '#2910de'],
+      }
     ],
     labels: []
-};
-  constructor(private http: HttpClient) {}
+  };
+
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
     this.http.get('http://localhost:3000/budget')
-    .subscribe((res: any) => {
-      for (var i = 0; i < res.myBudget.length; i++) {
-        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
-        this.dataSource.labels[i] = res.myBudget[i].title;
-
-    }
-    this.createChart();
-    });
+      .subscribe((res: any) => {
+        for (let i = 0; i < res.myBudget.length; i++) {
+          this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+          this.dataSource.labels[i] = res.myBudget[i].title;
+        }
+        this.createChart();
+      });
   }
+
   createChart() {
-        let chartStatus = Chart.getChart('myChart');
-        if (chartStatus != undefined) {
-          chartStatus.destroy();
-        }
-        var ctx = <HTMLCanvasElement>document.getElementById('myChart');
-        var myPieChart = new Chart(ctx, {
-          type: 'pie',
-          data: this.dataSource
-        });
-      }
+    if (isPlatformBrowser(this.platformId)) {
+      let chartStatus = Chart.getChart('myChart');
+      if (chartStatus != undefined) {
+        chartStatus.destroy();
+      }
+      var ctx = <HTMLCanvasElement>document.getElementById('myChart');
+      var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: this.dataSource
+      });
+    }
+  }
 }
-
-
-
-
-
